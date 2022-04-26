87c4366c78149e124adb26b809537d5ddc76bc28
aotten <alina.otten@twt.de>
2022-04-26 11:47:55 +0200

    finish home page styling

--- pages/components/compareLayout.tsx
+++ pages/components/compareLayout.tsx
@@ -2,27 +2,36 @@ import React from "react";
 import Image from "next/image";
 
 type CompareLayoutProps = {
-  imageSrc: string;
-  imageWidth: number;
-  imageHeight: number;
+  imageSrc?: string;
+  imageWidth?: number;
+  imageHeight?: number;
   content: string;
   imageFirst?: boolean;
 }
-export default function CompareLayout({imageSrc, imageWidth, imageHeight, content, imageFirst = true}: CompareLayoutProps) {
+export default function CompareLayout({
+                                        imageSrc,
+                                        imageWidth = 100,
+                                        imageHeight = 100,
+                                        content,
+                                        imageFirst = true,
+                                        children
+                                      }: React.PropsWithChildren<{}> & CompareLayoutProps) {
   return (
     <>
       <div className="flex items-center justify-center flex-wrap w-3/5 mt-24">
         {imageFirst ?
           <>
-            <div className="w-1/2 flex items-center justify-center">
-              <Image src={imageSrc} alt="Mouse Scroll Vector" width={imageWidth} height={imageHeight}/>
+            <div className="w-1/2 flex items-center justify-center relative">
+              {imageSrc && <Image src={imageSrc} alt="Compare Image" width={imageWidth} height={imageHeight}/>}
+              {children}
             </div>
             <h2 className="w-1/2 font-medium flex items-center justify-center">{content}</h2>
           </> :
           <>
             <h2 className="w-1/2 font-medium flex items-center justify-center">{content}</h2>
             <div className="w-1/2 flex items-center justify-center">
-              <Image src={imageSrc} alt="Mouse Scroll Vector" width={imageWidth} height={imageHeight}/>
+              {imageSrc && <Image src={imageSrc} alt="Compare Image" width={imageWidth} height={imageHeight}/>}
+              {children}
             </div>
           </>
         }
